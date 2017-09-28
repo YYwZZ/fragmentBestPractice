d45ccd1cd00b1993edb7ee4127150b68d3cd6ffe
YYwZZ <1195071851@qq.com>
2017-09-28 10:22:26 +0800

    FirstCommit

---
+++ app/src/main/java/app/wyyzzz/fragmentbestpractice/NewsTitleFragment.java
@@ -0,0 +1,89 @@
+package app.wyyzzz.fragmentbestpractice;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by wuyuanyang on 28/09/2017.
+ */
+
+public class NewsTitleFragment extends Fragment {
+
+    private boolean isTwoPane;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater ,ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.news_title_frag,container,false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        if(getActivity().findViewById(R.id.news_content_layout)!=null){
+            isTwoPane = true;//可以找到news_content_layout布局时为双页模式
+        }else{
+            isTwoPane = false;//找不到news_content_layout布局时，为单页模式
+        }
+    }
+
+    class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder>{
+
+        private List<News> mNewsList;
+
+        class ViewHolder extends RecyclerView.ViewHolder{
+
+            TextView newsTitleText;
+
+            public ViewHolder(View view){
+                super(view);
+                newsTitleText = (TextView) view.findViewById(R.id.news_title);
+            }
+        }
+
+        public NewsAdapter(List<News> newsList){
+            mNewsList = newsList;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent,int viewType){
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_item,parent,false);
+            final ViewHolder holder = new ViewHolder(view);
+            view.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    News news = mNewsList.get(holder.getAdapterPosition());
+                    if(isTwoPane){
+                        //如果是双页模式，则刷新NewsContentFragment
+                        NewsContentFragment newsContentFragment =
+                                (NewsContentFragment)getFragmentManager().findFragmentById(R.id.news_content_fragment);
+                        newsContentFragment .refresh(news.getTitle(),news.getContent());
+                    }else{
+                        //如果是单页模式，则直接启动NewsContentActivity
+                        NewsContentActivity.actionStart(getActivity(),news.getTitle(),news.getContent());
+                    }
+                }
+            });
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder,int position){
+            News news = mNewsList.get(position);
+            holder.newsTitleText.setText(news.getTitle());
+        }
+
+        @Override
+        public int getItemCount(){
+            return mNewsList.size();
+        }
+
+    }
+}
